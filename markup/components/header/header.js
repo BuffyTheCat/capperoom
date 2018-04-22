8fde3f1be5e338cc389716a38c312069f414681e
Svyatoslav Home <svt93@bk.ru>
2018-04-22 13:27:23 +0300

    header end

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,3 +1,18 @@
 $('.header__sublinks').mCustomScrollbar({
     axis: 'y'
 });
+
+$('.search__button').on('click', function () {
+    $('.header__link').hide();
+    $('.hamburger').show();
+    $('.search__input').show();
+    $('.search__input').addClass('active');
+});
+
+
+$('.hamburger').on('click', function () {
+    $('.search__input').hide();
+    $('.search__input').removeClass('active');
+    $('.hamburger').hide();
+    $('.header__link').show();
+});
